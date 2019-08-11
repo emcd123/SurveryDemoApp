3d7a5de6cf4e269bb38dcd2b6e8d9ba74a70b69b
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-08-11 17:24:04 +0100

    created a mvc web app

---
+++ SurveyDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SurveyDemoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
