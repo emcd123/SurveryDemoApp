0b938163f7bec55ae7e985f350e01da51f7e8620
Owen McDonnell <owen.mcdonnell@hpe.com>
2019-08-12 14:06:36 +0100

    Seeded data to the database for questions. CRUD operations should be operational

---
+++ SurveyDemoApp/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyDemoApp.Models;
+using System;
+using System.Linq;
+
+namespace SurveyDemoApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SurveyDemoAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<SurveyDemoAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.Question.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Question.AddRange(
+                    new Question
+                    {
+                        QuestionText = "Did you make this with MVC?",
+                        AskingDate = DateTime.Parse("1989-2-12"),
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
