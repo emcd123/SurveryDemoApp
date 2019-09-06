c52684039c1ce292346d181990e26f94819e3cec
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-09-06 17:25:12 +0100

    Added survey id column to Submissions table

---
+++ SurveyDemoApp/Migrations/20190906162401_NewColumnSubmissionTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SurveyDemoApp.Migrations
+{
+    public partial class NewColumnSubmissionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SurveyId",
+                table: "Submission",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SurveyId",
+                table: "Submission");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+    }
+}
